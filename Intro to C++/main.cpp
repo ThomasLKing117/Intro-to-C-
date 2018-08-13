e33ccb7d97cd7a85b368813503ddb4e9575dd704
Holiday Course <holiday@ad.aie.edu>
2018-08-13 16:27:56 -0500

    Add project files.

---
+++ Intro to C++/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main()
+{
+	std::cout << "Thomas King" << std::endl;
+	std::cout << "Green Apples" << std::endl;
+	std::cout << "Kiwi" << std::endl;
+	std::cout << "Oranges\n" <<std::endl;
+	std::cout << "1.In the End\n2.Waiting for the End\n3.Castle of Glass\n4.Numb\n5.Heavy\n6.Lucid Dream\n7.Location\n8.Young Dumb and Broke\n9.God's Plan\n10.Drowning\n" << std::endl;
+	std::cout << "0\n1\n2\n3\n4\n5\n" << std::endl;
+	std::cout << "5\n4\n3\n2\n1\n0\n" << std::endl;
+	std::cout << "\1" << std::endl;
+	std::cout << "\2" << std::endl;
+	std::cout << "\3" << std::endl;
+	std::cout << "\4" << std::endl;
+	std::cout << "\5" << std::endl;
+	system("pause");
+}
